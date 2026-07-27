c5be713bb4df814a7e9261f73a71f7482007160f
kavyakusuma875-gif <kavyakusuma875@gmail.com>
2026-07-27 22:35:56 +0530

    Add leaderboard route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -174,31 +174,22 @@ router.get("/points/:email", async (req, res) => {
 =========================== */
 
 router.get("/leaderboard", async (req, res) => {
-
     try {
 
-        const users = await User.find().sort({
-
-            points: -1
-
-        });
+        const users = await User.find({})
+            .sort({ points: -1 })
+            .select("name points");
 
         res.json(users);
 
-    }
-
-    catch (error) {
+    } catch (error) {
 
         res.status(500).json({
-
             message: error.message
-
         });
 
     }
-
 });
-
 
 /* ===========================
    ADD BONUS POINTS
